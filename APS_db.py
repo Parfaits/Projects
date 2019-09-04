3c8fcc2c6546643105be79e4a0d3188221ecdb2d
Parfaits <36318895+Parfaits@users.noreply.github.com>
2019-09-03 20:06:36 -0700

    Update APS_db.py
    -No more naming your own database
    +more prints

--- APS_db.py
+++ APS_db.py
@@ -3,18 +3,21 @@ import math
 
 class APS_db():
 	"""Handle sqlite3 database operations for AmazonPriceStalker"""
-	_conn = sqlite3.connect("APS_client.db")
+	try:
+		_conn = sqlite3.connect("APS_client.db")
+		print("[DB] Connected to DB.")
+	except sqlite3.Error as e:
+		print("[DB] Problem connecting to DB.")
 	_cur = _conn.cursor()
 
-	def __init__(self, user, email=None, price=None, date=None):
-		self.user = user
+	def __init__(self, email, price=None, date=None):
 		self.email = email
 		self.price = price
 		self.date = date
 
 	def _stdev(self, avg):
 		# Sample standard deviation of the price over time.
-		self._cur.execute(f"SELECT {self.price} FROM {self.user};")
+		self._cur.execute(f"SELECT {self.price} FROM '{self.email}';")
 		p = self._cur.fetchall()
 		priceList = []
 		for x in p:
@@ -28,33 +31,47 @@ class APS_db():
 		return sd
 
 	def createClient(self):
-		self._cur.execute(f"CREATE TABLE IF NOT EXISTS {self.user} (email text, price real, datestamp text);")
+		self._cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.email}';")
+		exists = self._cur.fetchone()
+		if exists:
+			print(f"[DB] Client's table exists for {self.email}.")
+		else:
+			print("[DB] Created client's table.")
+		self._cur.execute(f"CREATE TABLE IF NOT EXISTS '{self.email}' (price REAL, datestamp TEXT);")
 		self._conn.commit()
 
 	def insertValue(self):
-		self._cur.execute(f"INSERT INTO {self.user} VALUES ('{self.email}', {self.price}, '{self.date}');")
+		self._cur.execute(f"INSERT INTO '{self.email}' VALUES ({self.price}, '{self.date}');")
 		self._conn.commit()
+		print("[DB] Inserted data into client's table.")
 
 	def statistics(self):
-		self._cur.execute(f"SELECT AVG(price) FROM {self.user};")
-		avg = self._cur.fetchone()
-		self._cur.execute(f"SELECT MAX(price) FROM {self.user};")
-		maxPrice = self._cur.fetchone()
-		self._cur.execute(f"SELECT MIN(price) FROM {self.user};")
-		minPrice = self._cur.fetchone()
-		self._cur.execute(f"SELECT COUNT(*) FROM {self.user};")
+		self._cur.execute(f"SELECT COUNT(*) FROM '{self.email}';")
 		numEntries = self._cur.fetchone()
-		return (minPrice[0], maxPrice[0], numEntries[0], round(avg[0], 2), round(self._stdev(avg[0]), 2))
+		if numEntries[0] <= 1:
+			print("[DB] Not enough entries.")
+			return None
+		self._cur.execute(f"SELECT AVG(price) FROM '{self.email}';")
+		avg = self._cur.fetchone()
+		self._cur.execute(f"SELECT MAX(price) FROM '{self.email}';")
+		maxPrice = self._cur.fetchone()
+		self._cur.execute(f"SELECT MIN(price) FROM '{self.email}';")
+		minPrice = self._cur.fetchone()
+		stats = (numEntries[0], minPrice[0], maxPrice[0], round(avg[0], 2), round(self._stdev(avg[0]), 2))
+		print(f"[DB] Statistics for client {self.email}:")
+		print("#Entries\tminPrice\tmaxPrice\tAverage\tStdev")
+		print(f"{stats[0]}\t\t{stats[1]}\t\t{stats[2]}\t\t{stats[3]}\t{stats[4]}")
+		return stats
 
 	def fetch(self, option='*', n=0):
 		if option == '*':
-			self._cur.execute(f"SELECT * FROM {self.user};")
+			self._cur.execute(f"SELECT * FROM '{self.email}';")
 			return self._cur.fetchall()
 		elif option == 1:
-			self._cur.execute(f"SELECT * FROM {self.user};")
+			self._cur.execute(f"SELECT * FROM '{self.email}';")
 			return self._cur.fetchone()
 		elif option == '+' and n > 0:
-			self._cur.execute(f"SELECT * FROM {self.user};")
+			self._cur.execute(f"SELECT * FROM '{self.email}';")
 			return self._cur.fetchmany(n)
 		elif option == 't':
 			self._cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -63,22 +80,27 @@ class APS_db():
 			print("[DB] option unavailable.")
 
 	def update(self, newPrice):
-		self._cur.execute(f"UPDATE {self.user} SET price = {newPrice} WHERE datestamp = {self.date};")
+		self._cur.execute(f"UPDATE '{self.email}' SET price = {newPrice} WHERE datestamp = {self.date};")
 		self._conn.commit()
+		print("[DB] Updated client's table.")
 
 	def removeRow(self, thisDate):
-		self._cur.execute(f"DELETE FROM {self.user} WHERE datestamp = {thisDate};")
+		self._cur.execute(f"DELETE FROM '{self.email}' WHERE datestamp = {thisDate};")
 		self._cur.execute("VACUUM;")
 		self._conn.commit()
+		print(f"[DB] Removed client's row at {thisDate}.")
 
-	def removeClient(self, thisUser):
-		self._cur.execute(f"DROP TABLE IF EXISTS {thisUser};")
+	def removeClient(self, thisEmail):
+		self._cur.execute(f"DROP TABLE IF EXISTS '{thisEmail}';")
 		self._cur.execute("VACUUM;")
 		self._conn.commit()
+		print(f"[DB] Removed client {thisEmail} table.")
 
 	def clearTable(self):
-		self.removeClient(self.user)
-		self.createClient(self.user)
+		self.removeClient(self.email)
+		self.createClient(self.email)
+		print("[DB] Cleared client's table.")
 
 	def closeConnection(self):
 		self._conn.close()
+		print("[DB] Connection to database closed.")
